50b574c7d9b4cf28edb216ef122b66460e3f1dfb
yangjiayi <1335823608@qq.com>
2016-09-23 00:33:18 +0800

    update

---
+++ Crawler/ac_video.py
@@ -0,0 +1,95 @@
+# -*- encoding=utf-8 -*-
+"""
+    python: 3.5
+    author: yjy
+    date: 2016-9-22
+    desc: 抓取acfun网站的所有视频信息
+"""
+import urllib.parse
+import urllib.request
+import save_data
+import re
+import threading
+
+
+class AcFun:
+    def __init__(self, start_id, end_id):
+        self.agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 \
+                    (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+        self.proxy_handler = urllib.request.ProxyHandler(None)
+        self.proxy_auth_handler = urllib.request.ProxyBasicAuthHandler
+        self.opener = urllib.request.build_opener(self.proxy_handler, self.proxy_auth_handler)
+        self.opener.addheaders = [
+                ('User-Agent', self.agent)
+            ]
+        self.start_id = start_id
+        self.end_id = end_id
+
+    def check_exist(self):
+        global end_threads
+        while self.start_id <= self.end_id:
+            id = self.start_id
+            self.start_id += 1
+            print('正在处理第', id, '个视频')
+            try:
+                url = 'http://www.acfun.tv/v/ac'+str(id)
+                response = self.opener.open(url)
+                data = response.read().decode()
+                if data and 'title' in data:
+                    data_str = urllib.parse.unquote(str(data))
+                    res = re.search(r'<title>(.*)</title>', data_str)
+                    if res.group(1):
+                        video = {'id': id, 'video_name': res.group(1), 'link': url}
+                        save_data.save(video, False)
+                        self.get_data(id)
+            except:
+                pass
+        else:
+            end_threads += 1
+            result()
+
+    def get_data(self, id):
+        try:
+            url = 'http://www.acfun.tv/content_view.aspx?contentId='+str(id)
+            response = self.opener.open(url)
+            data = response.read().decode()
+            if data:
+                data_list = eval(data)
+                video = {'id': id,
+                         'collect_num': data_list[5],
+                         'view_num': data_list[0],
+                         'dan_num': data_list[2],
+                         'banana_num': data_list[6],
+                         'comment_num': data_list[1]
+                         }
+                save_data.save(video, True)
+        except:
+            pass
+
+
+def result():
+    if end_threads == create_threads:
+        print('结束了')
+
+
+def start(start_id, end_id, thread_num):
+    global create_threads
+    i = 0
+    threads = []
+    af = AcFun(start_id, end_id)
+    try:
+        while i < thread_num:
+            thread = threading.Thread(target=af.check_exist, args={})
+            thread.start()
+            threads.append(thread)
+            i += 1
+    except Exception as e:
+        print('出错了', e)
+    finally:
+        create_threads = len(threads)
+
+create_threads = 0;
+end_threads = 0
+start(1, 3500000, 100)
+
+
--- MySQL/test.py
+++ MySQL/test.py
@@ -9,23 +9,17 @@ import pymysql
 conn = pymysql.connect(
     host='localhost',
     port=3306,
-    user='root',
-    passwd='1234',
-    db='python',
+    user='yjy',
+    passwd='yyyyyy',
+    db='yjy',
     charset='utf8'
     )
 
 cur = conn.cursor()
 
-cur.execute('create table images (\
-            id int(10) , \
-            img_path varchar(255), \
-            add_date timestamp, \
-            name varchar(255), \
-            primary key(id) \
-            )')
+cur.execute('insert into ac_video (id) VALUES (1)')
 
+cur.execute("select * from ac_video")
+for data in cur:
+    print(str(data))
 
-# cur.execute("select * from n_user_info limit 0,20")
-# for data in cur:
-#     print(str(data))
