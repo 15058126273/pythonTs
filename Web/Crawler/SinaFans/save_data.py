85247f1180be308a39e059ea7fd26a05b9e6ccc7
yjy <1335823608@qq.com>
2016-11-21 18:18:43 +0800

    更新了 爬取新浪所有用户粉丝数

--- Web/Crawler/SinaFans/save_data.py
+++ Web/Crawler/SinaFans/save_data.py
@@ -8,11 +8,10 @@
 import pymysql
 
 
-def save(video, update):
+def save(sina):
     """
     保存 or 更新数据
-    :param video: 数据包
-    :param update: 是否是更新 true代表更新否则保存
+    :param sina: 数据包
     :return:
     """
     conn = pymysql.connect(
@@ -25,90 +24,27 @@ def save(video, update):
     )
     cur = conn.cursor()
     try:
-        video_id = video.get('id')
-        link = video.get('link')
-        video_name = video.get('video_name')
-        collect_num = video.get('collect_num')
-        view_num = video.get('view_num')
-        comment_num = video.get('comment_num')
-        dan_num = video.get('dan_num')
-        coin_num = video.get('coin_num')
-        share_num = video.get('share_num')
-        if update:
-            sql = 'update bi_video set CHANGE where id = '+str(video_id)
-            change = ''
-            value = []
-            if link is not None:
-                change += 'link=%s,'
-                value.append(link)
-            if video_name is not None:
-                change += 'video_name=%s,'
-                value.append(video_name)
-            if collect_num is not None:
-                change += 'collect_num=%s,'
-                value.append(collect_num)
-            if view_num is not None:
-                change += 'view_num=%s,'
-                value.append(view_num)
-            if comment_num is not None:
-                change += 'comment_num=%s,'
-                value.append(comment_num)
-            if dan_num is not None:
-                change += 'dan_num=%s,'
-                value.append(dan_num)
-            if coin_num is not None:
-                change += 'coin_num=%s,'
-                value.append(coin_num)
-            if share_num is not None:
-                change += 'share_num=%s,'
-                value.append(share_num)
-            if change.endswith(','):
-                change = change[:-1]
-            sql = sql.replace('CHANGE', change)
-            cur.execute(sql, tuple(value))
-        else:
-            sql = 'insert into bi_video (COLUMN) values (VALUES)'
-            column = ''
-            values = ''
-            value = []
-            if video_id is not None:
-                column += 'id'
-                value.append(video_id)
-                values += '%s'
-            if link is not None:
-                column += ',link'
-                value.append(str(link))
-                values += ',%s'
-            if video_name is not None:
-                column += ',video_name'
-                value.append(str(video_name))
-                values += ',%s'
-            if collect_num is not None:
-                column += ',collect_num'
-                values += ',%s'
-                value.append(str(collect_num))
-            if view_num is not None:
-                column += ',view_num'
-                values += ',%s'
-                value.append(str(view_num))
-            if comment_num is not None:
-                column += ',comment_num'
-                values += ',%s'
-                value.append(str(comment_num))
-            if dan_num is not None:
-                column += ',dan_num'
-                values += ',%s'
-                value.append(str(dan_num))
-            if coin_num is not None:
-                column += ',coin_num'
-                values += ',%s'
-                value.append(str(coin_num))
-            if share_num is not None:
-                column += ',share_num'
-                values += ',%s'
-                value.append(str(share_num))
-            sql = sql.replace('COLUMN', column).replace('VALUES', values)
-            cur.execute(sql, tuple(value))
+        sina_id = sina.get('id')
+        fans_num = sina.get('fans_num')
+        nick_name = sina.get('nick_name')
+        sql = 'insert into sina_fans (COLUMN) values (VALUES)'
+        column = ''
+        values = ''
+        value = []
+        if id is not None:
+            column += 'id'
+            value.append(sina_id)
+            values += '%s'
+        if fans_num is not None:
+            column += ',fans_num'
+            value.append(fans_num)
+            values += ',%s'
+        if nick_name is not None:
+            column += ',nick_name'
+            value.append(str(nick_name))
+            values += ',%s'
+        sql = sql.replace('COLUMN', column).replace('VALUES', values)
+        cur.execute(sql, tuple(value))
         conn.commit()
     except:
         pass
--- Web/Crawler/SinaFans/sina_fans.py
+++ Web/Crawler/SinaFans/sina_fans.py
@@ -8,12 +8,23 @@
 import requests
 import re
 import socket
-
+import threading
+import save_data
+import urllib.request
+socket.setdefaulttimeout(3)
+requests.adapters.DEFAULT_RETRIES = 5
+s = requests.session()
+s.keep_alive = False
 
 class SinaFans:
-    def __init__(self):
-        self.headers = {
-            "Cookie": "YF-Ugrow-G0=3a02f95fa8b3c9dc73c74bc9f2ca4fc6; \
+    def __init__(self, id, allid):
+        self.id = id
+        self.allid = allid
+        self.proxy_handler = urllib.request.ProxyHandler()
+        self.proxy_auth_handler = urllib.request.ProxyBasicAuthHandler
+        self.opener = urllib.request.build_opener(self.proxy_handler, self.proxy_auth_handler)
+        self.opener.addheaders = [
+            ('Cookie', 'YF-Ugrow-G0=3a02f95fa8b3c9dc73c74bc9f2ca4fc6; \
             SUB=_2AkMvck_vf8NhqwJRmP4UymLibox3zwzEieLBAH7sJRMxHRl-yT83qn0StRBHsQfwSfDmhx2jCrwvYqn_oUwvPQ..; \
             SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9WFe7ir_.E1hmCsrzF76ZV7c; \
             login_sid_t=2011850be0affbbcf60024c2dc53a088; \
@@ -22,16 +33,25 @@ class SinaFans:
             YF-Page-G0=e1a5a1aae05361d646241e28c550f987; _s_tentry=-; \
             Apache=2637200928293.1685.1479459032669; \
             SINAGLOBAL=2637200928293.1685.1479459032669; \
-            ULV=1479459032712:1:1:1:2637200928293.1685.1479459032669:"
-        }
+            ULV=1479459032712:1:1:1:2637200928293.1685.1479459032669:')
+        ]
+
+    def start(self):
+        while 1:
+            self.id += 1
+            if self.id < self.allid:
+                id = self.id
+                self.catch_sb(id)
+            else:
+                break
 
     def catch_sb(self, id):
-        socket.setdefaulttimeout(5)
-        param = {'ajwvr': 6, 'id': id, 'type': 1}
-        res = requests.get("http://weibo.com/aj/v6/user/newcard", params=param, headers=self.headers)
-        print(res.status_code)
-        if res.status_code == 200:
-            data = res.text
+        try:
+            if id % 1000 == 0:
+                print('获取id ------>', id)
+            response = self.opener.open("http://weibo.com/aj/v6/user/newcard?id="+str(id), timeout=socket.getdefaulttimeout())
+            # res = requests.get("http://weibo.com/aj/v6/user/newcard", params=param, headers=self.headers)
+            data = response.read().decode()
             regexpr = re.compile(r'&nick=(.*)\\\" suda-uatrack=\\\"key=(.*)&value=privateletter\\\">', re.DOTALL)
             regexpr2 = re.compile(r'class=\\\"num W_fb\\\">(.*)<\\/em><\\/a><\\/span>\\n        <span', re.DOTALL)
             m = regexpr.search(data)
@@ -39,19 +59,23 @@ class SinaFans:
             if m and m2:
                 fans = m2.group(1).encode().decode("unicode-escape")
                 fans = fans.replace("万", '0000')
-                print(m.group(1).encode().decode("unicode-escape"), "：", int(fans))
+                nick_name = m.group(1).encode().decode("unicode-escape")
+                save_data.save({'id': id, 'fans_num': int(fans), 'nick_name': nick_name})
             else:
-                print("获取用户",id,"的信息时失败")
-        else:
-            print("读取失败：" + res.status_code)
+                # print("获取用户",id,"的信息时失败")
+                pass
+        except Exception as e:
+            print("获取用户", id, "的信息时出错:", e)
 
 if "__main__" == __name__:
-    sina = SinaFans()
-    id = 3
+    THREADS = []
+    threads_num = 100
+    i = 0
+    id = 638712
     allid = 10000000000
-    while id < allid:
-        try:
-            sina.catch_sb(id)
-        except Exception as e:
-            print("获取用户",id,"的信息时出错:",e)
-        id += 1
+    sina = SinaFans(id, allid)
+    while i < threads_num:
+        thread_i = threading.Thread(target=sina.start, args={})
+        thread_i.start()
+        THREADS.append(thread_i)
+        i += 1
