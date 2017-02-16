16fa9a0233156096161374e8c90d566a5bf17a64
yangjiayi <1335823608@qq.com>
2017-02-16 08:35:33 +0800

    update

--- Web/CheckDomain/tokenCode.py
+++ Web/CheckDomain/tokenCode.py
@@ -1,6 +1,6 @@
 
 checkapi = "https://checkapi.aliyun.com/check/checkdomain"
-token = "check-web-hichina-com:ow8hqrqct0i9zo5h4z756ve9zwkp8esy"
+token = "check-web-hichina-com:7heczehbi1zi35df3r1b4xlanifxliua"
 headers = {
     'cookie': 'cna=PJo8D0/MI0ACAX17Z0t8aWd1',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
